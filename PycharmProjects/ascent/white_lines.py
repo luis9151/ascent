9f42f5d086b22bc3f67cd7799068a66207b1184c
Luis <luis9151@gmail.com>
2017-10-15 05:09:14 -0700

    initial commit

--- PycharmProjects/ascent/white_lines.py
+++ PycharmProjects/ascent/white_lines.py
@@ -20,10 +20,12 @@ def rgb_2_bw(im_rgb):
     return np.uint8(img)
 
 
-def img_pre_process(img):
-    edges = cv2.GaussianBlur(img, (3, 3), 0)
-    edges = cv2.Canny(edges, 150, 200)
-    edges = cv2.Sobel(edges, -1, 1, 0)
+def img_pre_process(edges):
+    k = 3
+    #edges = cv2.GaussianBlur(edges, (k, k), 0)
+    #edges = cv2.Canny(edges, 150, 200,L2gradient=False)
+    #edges = cv2.Canny(edges, 50, 150,L2gradient=False)
+    edges = cv2.Sobel(edges, -1, 1, 0, ksize=5)
     cv2.imwrite(dir + '/' + 'canny.png', edges)
     return edges
 
@@ -46,7 +48,7 @@ def draw_template(img):
     template = np.zeros(img.shape)
     for corner in corners:
         o1 = 25
-        o2 = 400
+        o2 = 500
         if corner[1]<center[1]:
             o1 = o1*-1
             o2 = o2 * -1
@@ -64,27 +66,96 @@ def templ_matching(lanes,template,img):
     h,w = template.shape
     meth = 'cv2.TM_CCOEFF'
     method = eval(meth)
-    # Apply template Matching
     res = cv2.matchTemplate(lanes, template, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
+    top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
+    img = img.copy()
     cv2.rectangle(img, top_left, bottom_right, (255), 2)
     cv2.imwrite(dir + '/' + 'find_lane' + '.png', img)
+    return lanes[top_left[1]:bottom_right[1],top_left[0]:bottom_right[0]], (top_left,bottom_right)
+
+
+def align_lanes(img,edges,xy,n):
+    highlight = np.zeros(img[:,:,0].shape)
+    highlight[xy[0][1]:xy[1][1],xy[0][0]:xy[1][0]] = edges
+    img[:, :, 0] = np.clip(img[:,:,0] - highlight*255, 0, None)
+    img[:, :, 1] = np.clip(img[:,:,1] - highlight*255, 0, None)
+    img[:, :, 2] = np.clip(img[:,:,2] + highlight*255, None, 255)
+    #img[:, :, 0] = img[:,:,0] - highlight
+    #img[:, :, 1] = img[:,:,1] - highlight
+    #img[:, :, 2] = img[:,:,2] + highlight
+    cv2.imwrite(dir + '/' + 'final_img_' + str(n).zfill(3) + '.png', img)
+
+
+def gaussian_kernel(size, size_y=None):
+    size = int(size)
+    if not size_y:
+        size_y = size
+    else:
+        size_y = int(size_y)
+    x, y = np.mgrid[-size:size + 1, -size_y:size_y + 1]
+    g = np.exp(-(x ** 2 / float(size) + y ** 2 / float(size_y)))
+    g = g[int(size) + 1, int(size / 2) + 1:int(int(size) * 1.5) + 1]
+    #g = g[int(size)+1,int(size/2)+1:int(size)+1]
+    #g = np.concatenate((g,np.flip(g,axis=0)))
+    g = g/np.max(g) #/ g.sum()
+    #g = np.sqrt(g)
+    return g
+
+
+def filter(img):
+    f = np.fft.fft2(img)
+    fshift = np.fft.fftshift(f)
+
+    filter = np.zeros(img.shape)
+    center = int(img.shape[0]/2)
+    #w = int(img.shape[0]/2)
+    w = 20
+
+    g = gaussian_kernel(w*2)
+    g = np.repeat(g.reshape(g.size,1),img.shape[1],axis=1)
+
+    #a = np.ones((w*2,img.shape[1]))
+    M = cv2.getRotationMatrix2D((img.shape[1] / 2, img.shape[0] / 2),45, 1)
+    g = cv2.warpAffine(g, M, (img.shape[1], img.shape[0]))
+
+    #filter[center-w:center+w,:] = g
+    #filter[:,center - w:center + w] = g
+    filter = g
+    fshift = fshift*filter
+    magnitude_spectrum = 20 * np.log(np.abs(fshift))
+
+    a = np.fft.ifftshift(fshift)
+    a = np.fft.ifft2(a)
+    a = np.real(a)
+
+    plt.subplot(211), plt.imshow(a, cmap='gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(212), plt.imshow(magnitude_spectrum, cmap='gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+
+    return np.uint8(a)
 
 
 def main():
-    im_rgb = load_im(10)
-    img = rgb_2_bw(im_rgb)
-    edges = img_pre_process(img)
-    lanes = Hough_transform(edges, img,80,20,15,'lanes')
-    #bounds = Hough_transform(edges, img,200,1,50,'bounds')
-    template = draw_template(img)
-    templ_matching(lanes,template,img)
+    images = np.arange(20)
+    images = [0]
+    for n in images:
+        im_rgb = load_im(n)
+        img = rgb_2_bw(im_rgb)
+        img = filter(img)
+
+        edges = img_pre_process(img)
+        #lanes = Hough_transform(edges, img,80,20,15,'lanes')
+        lanes = Hough_transform(edges, img,50,50,50,'lanes')
+        #bounds = Hough_transform(edges, img,200,1,50,'bounds')
+        template = draw_template(img)
+        box,xy  = templ_matching(lanes,template,img)
+
+        #edges = img_pre_process(box)
+        #final = Hough_transform(edges, img,100,50,1,'final')
+        #align_lanes(im_rgb, final[:edges.shape[0],:edges.shape[1]], xy, n)
 
 
 
